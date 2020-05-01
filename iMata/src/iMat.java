b2ea6705db69e6f827fa0c561294ba3cda67af45
KarlssonLucas <Lucaskarlsson@live.se>
2020-05-01 14:46:54 +0200

    Added fxml file and fixed compiling issue

--- iMata/src/iMat.java
+++ iMata/src/iMat.java
@@ -1,5 +1,34 @@
-public class iMat {
-    public static void main(String[] args) {
-        System.out.println("a");
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class iMat extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        ResourceBundle bundle = java.util.ResourceBundle.getBundle("resources/iMat");
+
+        Parent root = FXMLLoader.load(getClass().getResource("iMat.fxml"), bundle);
+
+        Scene scene = new Scene(root, 800, 500);
+
+        stage.setTitle(bundle.getString("application.name"));
+        stage.setScene(scene);
+        stage.show();
+
+
     }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
