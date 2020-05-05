9deb958555a5dade3f26789ddc5d8d9d8cfc0eeb
Fille <54572646+ideefiix@users.noreply.github.com>
2020-05-05 18:26:27 +0200

    More UI fixes

--- iMata/src/iMat.java
+++ iMata/src/iMat.java
@@ -15,7 +15,7 @@ public class iMat extends Application {
 
         Parent root = FXMLLoader.load(getClass().getResource("iMat.fxml"), bundle);
 
-        Scene scene = new Scene(root, 1175, 850);
+        Scene scene = new Scene(root, 1175, 1000);
 
         stage.setTitle(bundle.getString("application.name"));
         stage.setScene(scene);
