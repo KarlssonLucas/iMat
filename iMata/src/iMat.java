e8d85d6a288d2488c84562b77c8e36f68b8e5f00
Fille <54572646+ideefiix@users.noreply.github.com>
2020-05-10 18:23:58 +0200

    "Mina sidor" fixes

--- iMata/src/iMat.java
+++ iMata/src/iMat.java
@@ -15,7 +15,7 @@ public class iMat extends Application {
 
         Parent root = FXMLLoader.load(getClass().getResource("iMat.fxml"), bundle);
 
-        Scene scene = new Scene(root, 1175, 840);
+        Scene scene = new Scene(root, 1175, 883);
 
         stage.setTitle(bundle.getString("application.name"));
         stage.setScene(scene);
--- iMata/src/minaSidorController.java
+++ iMata/src/minaSidorController.java
@@ -1,13 +1,42 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import se.chalmers.cse.dat216.project.CartEvent;
 import se.chalmers.cse.dat216.project.ShoppingCartListener;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class minaSidorController implements Initializable, ShoppingCartListener {
+
+    @FXML private Button minaSidorButton;
+    @FXML private Button checkoutButton;
+    @FXML private Button helpButton;
+    @FXML private Button butikButton;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    // NAVIGATION
+
+    public void butiken(ActionEvent event) throws IOException {
+
+        Parent butikenParent = FXMLLoader.load(getClass().getResource("iMat.fxml"));
+        Scene butikenScene = new Scene(butikenParent);
+
+        Stage butikenStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        butikenStage.setScene(butikenScene);
+        butikenStage.show();
 
     }
 
